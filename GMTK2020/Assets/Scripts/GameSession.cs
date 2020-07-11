e2f5d81332a53bd94cee959a9c91a7cf62a7f7a0
pjheric <pjheric1219@gmail.com>
2020-07-11 15:28:49 +0900

    Menu Functionality Complete

---
+++ GMTK2020/Assets/Scripts/GameSession.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSession : MonoBehaviour {
+    
+    
+    private void Awake()
+    {
+        SetUpSingleton(); 
+    }
+
+    private void SetUpSingleton()
+    {
+       if(FindObjectsOfType(GetType()).Length >1)
+        {
+            
+            Destroy(gameObject); 
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject); 
+        }
+    }
+    
+   
+    public void ResetGame()
+    {
+        Destroy(gameObject); 
+    }
+}
---
+++ GMTK2020/Assets/Scripts/Level.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+public class Level : MonoBehaviour {
+    [SerializeField] float delayTime = 2f;
+	public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0); 
+    }
+    public void LoadOptionsMenu()
+    {
+        SceneManager.LoadScene(1); 
+    }
+    public void LoadAccessibilityMenu()
+    {
+        SceneManager.LoadScene(3); 
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(2);
+        FindObjectOfType<GameSession>().ResetGame(); 
+    }
+    public void LoadGameOver()
+    {
+        StartCoroutine(WaitAndLoad()); 
+    }
+    IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(delayTime);
+        SceneManager.LoadScene(2);
+    }
+    public void QuitGame()
+    {
+        Application.Quit(); 
+    }
+    
+}
